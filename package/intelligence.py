e71498481f7e268e7e31b5e0f285b04541236070
Rebecca Blixt <rebecca.blixt@hotmail.com>
2025-10-13 11:43:15 +0200

    created intelligence class and player class

---
+++ package/intelligence.py
@@ -0,0 +1,14 @@
+class Intelligence:
+    # Comp act as player
+    # Select difficulty level
+    def __init__(self):
+        pass
+        #self.level = level
+
+    # def update_difficulty():
+
+    # def level_easy():
+    
+    #def level_normal():
+    
+    # def level_hard(): 
