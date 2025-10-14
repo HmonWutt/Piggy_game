8af54e73dd91eb27c1310013fda848721ed301a6
Rebecca Blixt <rebecca.blixt@hotmail.com>
2025-10-14 11:23:22 +0200

    add intelligence interface. add comments re: future code needs to player.py

---
+++ package/intelligence_interface.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+class Inteligence(ABC):
+    
+    @abstractmethod
+    def decide(self, turn_score: int, total_score: int, opponent_score: int) -> str:
+        # AI decides the next move based on: 
+        # turn_score = the score for the current round
+        # total_score = the total score for the current game
+        # -> str = method returns a string
+        pass
+
+    
--- package/player.py
+++ package/player.py
@@ -1,3 +1,16 @@
+# import dice
+# import dicehand
+# import highscore
+
+""""Class for player"""
 class Player:
-    def __init__(self):
-        pass
+    def __init__(self, player_name):
+        self.player_name = player_name
+        # self.dicehand = dicehand()
+        # self.highscore = highscore()
+
+    # rename player
+    def change_name(self):
+        self.player_name = input("Enter your new name: ")
+
+# 
