22e7eb080cea58e1951197d1131d24299504076b
HmonWutt <wtthumon@gmail.com>
2025-10-20 12:43:13 +0200

    added dechen's modules to init file

--- package/__init__.py
+++ package/__init__.py
@@ -1,3 +1,6 @@
 from .dice import Dice
 from .dice_interface import Dice_interface
 from .game import Game
+from .highscore import HighScore
+from .histogram import Histogram
+from .intelligence import Intelligence
