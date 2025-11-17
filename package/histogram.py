eca564ec76039fa270d6f204963b9efaf9c8eaf0
HmonWutt <wtthumon@gmail.com>
2025-11-17 23:46:59 +0100

    changed # to 🏆

--- package/histogram.py
+++ package/histogram.py
@@ -1,45 +1,42 @@
-""" Histogram class shows player wins and games played. """
+"""Histogram class shows player wins and games played."""
+
 from .histogram_interface import HistogramInterface
 from .highscore import HighScore
 
 
 class Histogram(HistogramInterface):
-    """ Represents ASCII Histogram for player statistics. """
+    """Represents ASCII Histogram for player statistics."""
 
     def __init__(self, highscore: HighScore):
-        """ Initializes Histogram with a HighScore object.
+        """Initializes Histogram with a HighScore object.
         Args:
-            highscore (HighScore): A HighScore object. """
+            highscore (HighScore): A HighScore object."""
         self.highscore = highscore
 
     def display_games_played(self):
-        """ Displays ASCII bar chart for games played per player. """
+        """Displays ASCII bar chart for games played per player."""
         players = self.highscore.get_all_players()
 
         sorted_players = sorted(
-            players.items(),
-            key=lambda item: item[1]["games_played"],
-            reverse=True
+            players.items(), key=lambda item: item[1]["games_played"], reverse=True
         )
         print("Games played:")
         for name, stats in sorted_players:
             count = stats["games_played"]
-            bar = "#" * count
+            bar = "🏆" * count
             print(f"{name:12} | {count:3} {bar}")
         print()
 
     def display_wins(self):
-        """ Display ASCII bar chart for wins per player. """
+        """Display ASCII bar chart for wins per player."""
         players = self.highscore.get_all_players()
 
         sorted_players = sorted(
-            players.items(),
-            key=lambda item: item[1]["wins"],
-            reverse=True
+            players.items(), key=lambda item: item[1]["wins"], reverse=True
         )
         print("Wins:")
         for name, stats in sorted_players:
             count = stats["wins"]
-            bar = "#" * count
+            bar = "🏆" * count
             print(f"{name:12} | {count:3} {bar}")
         print()
