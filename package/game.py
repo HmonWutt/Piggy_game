a9ce12d752f9dac68504774ae9c2329afada98db
HmonWutt <wtthumon@gmail.com>
2025-11-21 18:02:14 +0100

    reformatted with black

--- package/game.py
+++ package/game.py
@@ -114,11 +114,11 @@ class Game(cmd.Cmd):
 
         return wrapper
 
-    def set_is_new_game(self, is_new_game): # pragma: no cover
+    def set_is_new_game(self, is_new_game):  # pragma: no cover
         """Set if this is a new game or restored from the saved game."""
         self.is_new_game = is_new_game
 
-    def preloop(self): # pragma: no cover
+    def preloop(self):  # pragma: no cover
         """Preloop.
 
         This will run before the game loop begins,
@@ -133,7 +133,7 @@ class Game(cmd.Cmd):
             self.start_game()
             self.display_score_board()
 
-    def choose_number_of_dice(self): # pragma: no cover
+    def choose_number_of_dice(self):  # pragma: no cover
         """Choose if one or two dice."""
         num_of_dice = inquirer.select(
             message="How many dice?", choices=["1", "2"]
@@ -142,7 +142,7 @@ class Game(cmd.Cmd):
             return 1
         return 2
 
-    def choose_robot_or_human(self): # pragma: no cover
+    def choose_robot_or_human(self):  # pragma: no cover
         """Choose if they want to play with a robot or a human."""
         action = inquirer.select(
             message="Play with?", choices=["🦾 a robot", "💪 a human"]
@@ -161,7 +161,7 @@ class Game(cmd.Cmd):
             self.is_opponent_robot = False
         self.start_game()
 
-    def choose_intelligence_level(self): # pragma: no cover
+    def choose_intelligence_level(self):  # pragma: no cover
         """Choose the IQ of the robot."""
         intel = inquirer.select(
             message="How smart do you want your robot friend to be?",
@@ -176,7 +176,7 @@ class Game(cmd.Cmd):
         return intelligence_levels["h"]
 
     @check_is_new_game
-    def do_start(self, arg): # pragma: no cover
+    def do_start(self, arg):  # pragma: no cover
         """Start the game."""
         if self.is_game_in_progress:
             print("Can't start a game while there is one in progress")
@@ -185,7 +185,7 @@ class Game(cmd.Cmd):
         self.number_of_dice = self.choose_number_of_dice()
         self.choose_robot_or_human()
 
-    def start_game(self): # pragma: no cover
+    def start_game(self):  # pragma: no cover
         """Start game or resume."""
         print("Game started")
         self.is_game_paused = False
@@ -195,14 +195,15 @@ class Game(cmd.Cmd):
 
     @check_is_active_game
     @check_is_game_paused
-    def do_play(self, arg): # pragma: no cover
+    def do_play(self, arg):  # pragma: no cover
         """Play. You will be asked to choose to roll or hold."""
         turn_score = 0
         points = self.current_player.get_score()
 
         print(
             f"\n{
-                self.current_player.get_name()}'s total points: {points}, Round total: {turn_score}")
+                self.current_player.get_name()}'s total points: {points}, Round total: {turn_score}"
+        )
 
         choice = self.choose_roll_or_hold()
         is_winner_found = False
@@ -241,7 +242,7 @@ class Game(cmd.Cmd):
                 self.auto_play()
 
     @check_is_game_paused
-    def auto_play(self): # pragma: no cover
+    def auto_play(self):  # pragma: no cover
         """Robot plays."""
         points = self.player_two.get_score()
         turn_score = 0
@@ -279,7 +280,7 @@ class Game(cmd.Cmd):
             if not is_winner_found:
                 self.pass_to_human()
 
-    def choose_roll_or_hold(self): # pragma: no cover
+    def choose_roll_or_hold(self):  # pragma: no cover
         """Choose to roll the dice or hold."""
         choice = inquirer.select(
             message="Roll or hold", choices=["Roll", "Hold"]
@@ -288,7 +289,7 @@ class Game(cmd.Cmd):
             return "Roll"
         return "Hold"
 
-    def roll(self): # pragma: no cover
+    def roll(self):  # pragma: no cover
         """Roll the dice once or twice depending on how many dice there are."""
         result_list = []
         result = 0
@@ -302,27 +303,30 @@ class Game(cmd.Cmd):
         print()
         return result, result_list
 
-    def count_ones(self, faces): # pragma: no cover
+    def count_ones(self, faces):  # pragma: no cover
         """Count the number of ones in the list "faces."""
         count = faces.count(1)
         return count
 
-    def print_rolled_one_outcome(self, num_of_ones): # pragma: no cover
+    def print_rolled_one_outcome(self, num_of_ones):  # pragma: no cover
         """Check how many ones have been rolled this round.
 
         To decide if the player had lost points from this round, points
         from this game, or no points at all.
         """
         if num_of_ones == 1:
-            print("Rolled a single one and lost all the points from this turn.")  # pragma: no cover
+            print(
+                "Rolled a single one and lost all the points from this turn."
+            )  # pragma: no cover
 
         if num_of_ones == 2:
-            print("Rolled double ones and lost all points from this game.")  # pragma: no cover
+            print(
+                "Rolled double ones and lost all points from this game."
+            )  # pragma: no cover
 
         print("TURN ENDS.\n")  # pragma: no cover
 
-
-    def switch_current_player(self): # pragma: no cover
+    def switch_current_player(self):  # pragma: no cover
         """Set current player to other player when current players turn ends.
 
         Change name in prompt.
@@ -333,12 +337,12 @@ class Game(cmd.Cmd):
             self.current_player = self.player_one
         Game.prompt = self.current_player.get_name() + "> "
 
-    def pass_to_human(self): # pragma: no cover
+    def pass_to_human(self):  # pragma: no cover
         """Change current player to human. Change name in prompt."""
         self.current_player = self.player_one
         Game.prompt = self.player_one.get_name() + "> "
 
-    def run_winner_found_sequence(self, winner): # pragma: no cover
+    def run_winner_found_sequence(self, winner):  # pragma: no cover
         """Announce the winner.
 
         Set that there is no active game;
@@ -354,13 +358,12 @@ class Game(cmd.Cmd):
         self.score_record.record_game(self.player_one, self.player_two, winner)
         self.reset_player_scores()
 
-    def reset_player_scores(self): # pragma: no cover
+    def reset_player_scores(self):  # pragma: no cover
         """Reset players scores."""
         self.player_two.set_score(0)
         self.player_one.set_score(0)
 
     def display_score_board(self):  # pragma: no cover
-
         """Display in game score board."""
         info = {
             self.player_one.get_name(): self.player_one.get_score(),
@@ -374,37 +377,37 @@ class Game(cmd.Cmd):
     """
         Utils.print_dict_table(info, banner)
 
-    def announce_winner(self, player): # pragma: no cover
+    def announce_winner(self, player):  # pragma: no cover
         """Announce the winner."""
         print(f"{player.get_name()} has won. Congrats🎉")
 
-    def announce_game_end(self): # pragma: no cover
+    def announce_game_end(self):  # pragma: no cover
         """Announce the end of game."""
         print("\nGAME OVER!. Type 'start' to play another game")
         Game.prompt = "piggygame> "
 
     @check_is_active_game
     @check_is_game_paused
-    def do_cheat(self, arg): # pragma: no cover
+    def do_cheat(self, arg):  # pragma: no cover
         """Cheat to win."""
         self.current_player.set_score(100)
         self.run_winner_found_sequence(self.current_player)
 
     @check_is_active_game
-    def do_pause(self, arg): # pragma: no cover
+    def do_pause(self, arg):  # pragma: no cover
         """Save game data to file and pause the game."""
         # self.save_game()
         self.is_game_paused = True
         print("Game paused. Type 'unpause' to resume game\n")
         Game.prompt = "piggygame> "
 
-    def do_explain(self, arg): # pragma: no cover
+    def do_explain(self, arg):  # pragma: no cover
         """Explain the rules of the game."""
         just_the_rules = Game.intro.splitlines()[4:14]
         print("\n".join(just_the_rules))
 
     @check_is_active_game
-    def do_unpause(self, arg): # pragma: no cover
+    def do_unpause(self, arg):  # pragma: no cover
         """Unpause the game."""
         self.is_game_paused = False
         self.display_score_board()
@@ -412,8 +415,7 @@ class Game(cmd.Cmd):
 
     @check_is_active_game
     @check_is_game_paused
-    def do_changename(self, arg): # pragma: no cover
-
+    def do_changename(self, arg):  # pragma: no cover
         """Change your username."""
         old_name = self.current_player.get_name()
         new_name = input("Enter your new name: ")
@@ -425,8 +427,7 @@ class Game(cmd.Cmd):
         self.score_record.update_player_name(old_name, new_name)
         Game.prompt = self.current_player.get_name() + "> "
 
-    def do_show(self, arg):# pragma: no cover
-
+    def do_show(self, arg):  # pragma: no cover
         """Show statistics."""
         choice = inquirer.select(
             message="What would you like to see?",
@@ -438,7 +439,7 @@ class Game(cmd.Cmd):
         else:
             stats.display_games_played()
 
-    def do_exit(self, arg): # pragma: no cover
+    def do_exit(self, arg):  # pragma: no cover
         """Exit the game."""
         choice = inquirer.select(
             message="Save the game to resume later?",
@@ -454,8 +455,7 @@ class Game(cmd.Cmd):
 
         return True
 
-    def __getstate__(self):# pragma: no cover
-
+    def __getstate__(self):  # pragma: no cover
         """Fix error.
 
         Added this to solve "can't pickle
@@ -470,8 +470,7 @@ class Game(cmd.Cmd):
                 del state[key]
         return state
 
-    def __setstate__(self, state):# pragma: no cover
-
+    def __setstate__(self, state):  # pragma: no cover
         """Fix error.
 
         Added this to solve "can't pickle
@@ -483,7 +482,7 @@ class Game(cmd.Cmd):
         self.stdin = sys.stdin
         self.stdout = sys.stdout
 
-    def postloop(self): # pragma: no cover
+    def postloop(self):  # pragma: no cover
         """Pickle the whole game object."""
         if self.save_game:
             with open("game_state.pkl", "wb") as f:
--- scripts/uml.py
+++ scripts/uml.py
@@ -1,18 +1,13 @@
 import os
 import subprocess
 
-os.makedirs('doc/uml', exist_ok=True)
-result = subprocess.run(['pyreverse',
-                         '-o',
-                         'png',
-                         '-p',
-                         'uml',
-                         'package/',
-                         '-d',
-                         'doc/uml/'],
-                        stdout=subprocess.DEVNULL,
-                        stderr=subprocess.DEVNULL)
+os.makedirs("doc/uml", exist_ok=True)
+result = subprocess.run(
+    ["pyreverse", "-o", "png", "-p", "uml", "package/", "-d", "doc/uml/"],
+    stdout=subprocess.DEVNULL,
+    stderr=subprocess.DEVNULL,
+)
 if result.returncode == 0:
-    print('✓ UML diagrams saved to doc/uml')
+    print("✓ UML diagrams saved to doc/uml")
 else:
-    print('✗ Uml generation failed')
+    print("✗ Uml generation failed")
